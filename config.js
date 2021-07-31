c6baa078e7b8ff8b090db97f5607102fd53c8587
Yurley Solimer <yurley.solimer@gmail.com>
2021-07-31 12:44:51 -0400

    DB connection

--- config.js
+++ config.js
@@ -4,5 +4,11 @@ module.exports = {
     },
     jwt: {
         SECRET: process.env.SECRET || 'secreto'
+    },
+    mysql: {
+        host: process.env.MYSQL_HOST || 'localhost',
+        user: process.env.MYSQL_USER || 'root',
+        password: process.env.MYSQL_PASSWORD || 'CaramitiE.23',
+        database: process.env.MYSQL_DB || 'prueba_db'
     }
 }
