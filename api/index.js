9cc0c71dcd2fa2814d8d064bd79321c96c3aa451
Yurley Solimer <yurley.solimer@gmail.com>
2021-08-04 01:07:49 -0400

    Posts como microservicio

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ const swaggerUi = require('swagger-ui-express');
 const config = require('../config.js');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
-const post = require('./components/post/network');
 const errors = require('../network/errors');
 
 const app = express();
@@ -18,11 +17,10 @@ const swaggerDoc = require('./swagger.json');
 // ROUER
 app.use('/api/user', user);
 app.use('/api/auth', auth);
-app.use('/api/post', post);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use(errors);
 
 app.listen(config.api.port, () => {
-    console.log('Api escuchando en el puerto ', config.api.port);
+    console.log('Servicio de Post escuchando en el puerto ', config.api.port);
 });
